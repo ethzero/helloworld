a07903cba8d79cd3fe2bb65d82fd2d7dff345435
ethzero <ethzero@ethzero.uk>
2020-10-24 22:09:52 +0100

    Addition of Redis DB

--- node_src/server.js
+++ node_src/server.js
@@ -3,6 +3,16 @@ require('console-stamp')(console, '[HH:MM:ss.l]');
 const express = require('express');
 const util = require("util");
 
+const redis = require("redis");
+const client = redis.createClient({
+  host: "172.25.0.3"
+});
+client.on("error", function(error) {
+  console.error(error);
+});
+client.set("key", "value", redis.print);
+client.get("key", redis.print);
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -16,6 +26,7 @@ app.get('/', (req, res) => {
   }
   res.send(`<pre>` + util.inspect($httpTransaction) + `</pre>`);
   console.log($httpTransaction);
+  client.set(new Date().toISOString(), JSON.stringify($httpTransaction), redis.print);
 });
 
 app.listen(PORT, HOST);
