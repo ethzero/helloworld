805aba8efd6b564b3462b1e861559c21f736470d
ethzero <ethzero@ethzero.uk>
2020-10-25 01:35:58 +0000

    Added update.sh

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Download the repo w/source
+REPO_TARGET=~/git/ethzero/helloworld
+mkdir -p $REPO_TARGET 2>/dev/null
+git clone https://github.com/ethzero/helloworld.git $REPO_TARGET 2>/dev/null
+
+# Take down the stake, update it, bring back up
+cd $REPO_TARGET
+git pull
+docker-compose down
+docker-compose pull
+docker-compose up --detach
+
+# Clean up
+docker image prune -f
+docker volume prune -f
